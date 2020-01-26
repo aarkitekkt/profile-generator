01dedb1e2fb5b0141046887e6db7aab451018a67
aarkitekkt <johnson.brycej@gmail.com>
2020-01-26 14:06:04 -0700

    build queryURL and query github API

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+const util = require("util");
+
+const appendFileAsync = util.promisify(fs.appendFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 const questions = [
     {
@@ -13,13 +18,42 @@ const questions = [
     }
 ];
 
-promptUser().then(function (response) {
-    console.log(response.github + "'s favorite color is " + response.color)
-});
+inquirer.prompt(questions)
+    .then(function (response) {
 
-function promptUser() {
-    return inquirer.prompt(questions);
+        console.log(response.github + "'s favorite color is " + response.color);
+
+        getGithub(response);
+
+    });
+
+
+function getGithub(response) {
+
+    let userName = response.github;
+
+    console.log("user " + userName + " is being searched for on Github")
+
+    const queryUrl = `https://api.github.com/users/${userName}`;
+
+    console.log(queryUrl);
+
+    axios
+        .get(queryUrl)
+        .then(function (res) {
+
+            const userData = res.data;
+
+            console.log(userData);
+
+        });
 }
+
+
+
+
+
+
 
 
 function writeToFile(fileName, data) {
