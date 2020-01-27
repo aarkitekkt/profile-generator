31083a39b2307d8755e5bb8ab8c7164acb012f70
aarkitekkt <johnson.brycej@gmail.com>
2020-01-26 17:16:11 -0700

    return data from guthub api

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const axios = require("axios");
 const util = require("util");
 
-const appendFileAsync = util.promisify(fs.appendFile);
-const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
     {
@@ -25,8 +24,10 @@ inquirer.prompt(questions)
 
         getGithub(response);
 
-    });
-
+    })
+    .catch(function (err) {
+        console.log(err);
+    });;
 
 function getGithub(response) {
 
@@ -36,27 +37,89 @@ function getGithub(response) {
 
     const queryUrl = `https://api.github.com/users/${userName}`;
 
-    console.log(queryUrl);
+    console.log(queryUrl)
 
     axios
         .get(queryUrl)
         .then(function (res) {
 
-            const userData = res.data;
+            userData = res.data;
 
-            console.log(userData);
+            console.log(userData.name);
+
+            writeFile(userData);
 
         });
 }
 
+function generateHTML(data) {
 
 
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="d-flex justify-content-center">
+        <div class="row">
+            <img class="rounded-circle" src="${data.avatar_url}" alt="">
+        </div>
+        <div class="row">
+            <h1>${data.name}</h1>
+        </div>
 
 
+    </div>
 
+</body>
 
+</html>`
+}
 
-function writeToFile(fileName, data) {
+function generateTextFile(data) {
+    const info = {
+        name: data.name,
+        location: data.location,
+        github: data.url,
+        profilePic: data.avatar_url,
+        repos: data.public_repos,
+        followers: data.followers,
+        following: data.following,
+    }
+
+    return info
+
+}
+
+function writeFile(data) {
+
+    console.log(data.name);
+
+    var fileName = data.name.toLowerCase() + ".html";
+    // 
+    const html = generateHTML(data);
+
+    // const text = generateTextFile(data);
+
+    writeFileAsync(fileName, html)
+        .then(function () {
+            console.log("successfully written .html file!")
+        });
+
+    // writeFileAsync(fileName, JSON.stringify(text))
+    //     .then(function () {
+    //         console.log("successfully wrote .txt file!")
+    //     });
 
 }
 
