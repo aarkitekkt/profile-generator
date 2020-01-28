cb9a34343762f53eae63e7a4ce2e7b663864453d
aarkitekkt <johnson.brycej@gmail.com>
2020-01-28 11:28:02 -0700

    retrieve data from api and generate user object

--- index.js
+++ index.js
@@ -5,57 +5,112 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [
-    {
-        type: "input",
-        name: "color",
-        message: "What is your favorite color?"
-    }, {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?"
-    }
-];
+const userInfo = [];
 
-inquirer.prompt(questions)
+askQuestions()
     .then(function (response) {
+        return showAnswers(response)
+    })
+    .then(function (userName) {
+        return buildQueryUrl(userName)
+    })
+    .then(function (queryUrl) {
+        return axios
+            .get(queryUrl)
+            .then(function (res) {
 
-        console.log(response.github + "'s favorite color is " + response.color);
+                const userData = res.data;
 
-        getGithub(response);
+                console.log("API call made for " + userData.login)
+
+                userInfo.name = userData.name;
+                userInfo.location = userData.location;
+                userInfo.followers = userData.followers;
+                userInfo.following = userData.following;
+                userInfo.gitHub = userData.html_url;
+                userInfo.profilePic = userData.avatar_url;
+                userInfo.repos = userData.public_repos;
+
+                return userInfo
+
+            })
+
 
     })
+    .then(function (userInfo) {
+
+        const starQueryUrl = `https://api.github.com/users/${userInfo.username}/starred`;
+
+        console.log(starQueryUrl);
+
+        return axios
+            .get(starQueryUrl)
+            .then(function (res) {
+
+                const starData = res.data;
+
+                userInfo.starCount = starData.length + " stars";
+
+                console.log(starData.length + " stars");
+
+                return userInfo
+            })
+    })
+    .then(function (userInfo) {
+        console.log(userInfo);
+    })
+    // .then(function (userData) {
+    //     return generateHTML(userData);
+    // })
+    // .then(function (userData, htmlFile) {
+    //     writeFile(userData, htmlFile)
+    // })
     .catch(function (err) {
         console.log(err);
-    });;
+    });
 
-function getGithub(response) {
+function askQuestions() {
 
-    let userName = response.github;
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "color",
+            message: "What is your favorite color?"
+        }, {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?"
+        }
+    ]);
+}
 
-    console.log("user " + userName + " is being searched for on Github")
+function showAnswers(data) {
+    let userName = data.github;
+    let color = data.color;
 
-    const queryUrl = `https://api.github.com/users/${userName}`;
+    console.log(userName + "'s favorite color is " + color);
 
-    console.log(queryUrl)
+    userInfo.username = userName;
 
-    axios
-        .get(queryUrl)
-        .then(function (res) {
+    userInfo.favoriteColor = color;
 
-            userData = res.data;
+    return userName
+}
 
-            console.log(userData.name);
+function buildQueryUrl(name) {
 
-            writeFile(userData);
+    const queryUrl = `https://api.github.com/users/${name}`;
 
-        });
+    console.log("searching " + queryUrl);
+
+    return queryUrl;
 }
 
 function generateHTML(data) {
 
+    var fileName = data.name
 
-    return `
+    var fileCode = `
     <!DOCTYPE html>
 <html lang="en">
 
@@ -70,20 +125,34 @@ function generateHTML(data) {
 </head>
 
 <body>
-    <div class="d-flex justify-content-center">
-        <div class="row">
-            <img class="rounded-circle" src="${data.avatar_url}" alt="">
-        </div>
-        <div class="row">
-            <h1>${data.name}</h1>
-        </div>
 
-
+    <!-- Profile Image and Name -->
+    <div class="d-flex">
+        <div class="row" style="background-color: red;">
+            <div class="col-6">
+                <img class="img-fluid rounded-circle my-5" src="${data.avatar_url}"
+                    alt="">
+            </div>
+            <div class="col-6 text-white text-justify-right">
+                <h3>${data.name}n</h3>
+                <h5>Location: ${data.location} </h5>
+                <h5>Github: ${data.html_url}</h5>
+                <ul>
+                    <li>Public Repos: ${data.public_repos}</li>
+                    <li>Followers: ${data.followers}</li>
+                    <li>Following: ${data.following}</li>
+                </ul>
+            </div>
+        </div>
     </div>
-
 </body>
 
 </html>`
+
+    console.log("html file generated for " + data.login)
+
+
+    return htmlFile
 }
 
 function generateTextFile(data) {
@@ -101,27 +170,27 @@ function generateTextFile(data) {
 
 }
 
-function writeFile(data) {
+// function writeFile(data) {
 
-    console.log(data.name);
+//     console.log(data.name);
 
-    var fileName = data.name.toLowerCase() + ".html";
-    // 
-    const html = generateHTML(data);
+//     var fileName = data.name.toLowerCase() + ".html";
+//     // 
+//     const html = generateHTML(file);
 
-    // const text = generateTextFile(data);
+//     // const text = generateTextFile(data);
 
-    writeFileAsync(fileName, html)
-        .then(function () {
-            console.log("successfully written .html file!")
-        });
+//     writeFileAsync(fileName, html)
+//         .then(function () {
+//             console.log("successfully written .html file!")
+//         });
 
-    // writeFileAsync(fileName, JSON.stringify(text))
-    //     .then(function () {
-    //         console.log("successfully wrote .txt file!")
-    //     });
+// writeFileAsync(fileName, JSON.stringify(text))
+//     .then(function () {
+//         console.log("successfully wrote .txt file!")
+//     });
 
-}
+// }
 
 function init() {
 }
